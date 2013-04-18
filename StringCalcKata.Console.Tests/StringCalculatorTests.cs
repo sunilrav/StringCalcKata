a15d747b6de4a0787c4c852f6cc39bfe3c95d05d
sunilrav <rsunilbabu@gmail.com>
2013-04-18 16:30:18 -0600

    first checkin

---
+++ StringCalcKata.Console.Tests/StringCalculatorTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalcKata.Console.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestMethod]
+        public void Add_EmptyString_ReturnZero()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+
+            //Act
+            var result = stringCalculator.Add("");
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Add_SingleNumber1_Returns1()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+
+            //Act
+            var result = stringCalculator.Add("1");
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Add_SingleNumber1091_Returns1091()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+
+            //Act
+            var result = stringCalculator.Add("1091");
+
+            //Assert
+            Assert.AreEqual(1091, result);
+        }
+
+        [TestMethod]
+        public void Add_TwoNumbers1And2_Returns3()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+
+            //Act
+            var result = stringCalculator.Add("1,2");
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Add_TwoNumbers50And51_Returns101()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+
+            //Act
+            var result = stringCalculator.Add("50,51");
+
+            //Assert
+            Assert.AreEqual(101, result);
+        }
+
+        [TestMethod]
+        public void Add_ThreeNumbers10And21And15_Returns46()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+
+            //Act
+            var result = stringCalculator.Add("10,21,15");
+
+            //Assert
+            Assert.AreEqual(46, result);
+        }
+
+        [TestMethod]
+        public void Add_ThreeNumbers10And21And15WithNewLineDemilter_Returns46()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+
+            //Act
+            var result = stringCalculator.Add("10\n21\n15");
+
+            //Assert
+            Assert.AreEqual(46, result);
+        }
+    }
+}
---
+++ StringCalcKata.Console/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalcKata.Console
+{
+    public class StringCalculator
+    {
+        public int Add(string delimitedNumbers)
+        {
+            if (String.IsNullOrEmpty(delimitedNumbers)) {
+                return 0;
+            }
+
+            var delimiters = new char[] {',', '\n'};
+            var positionOfDelimiter = delimitedNumbers.IndexOfAny(delimiters);
+            if (positionOfDelimiter < 0) {
+                return int.Parse(delimitedNumbers);
+            }
+
+            var delimiterUsed = delimitedNumbers.ToArray()[positionOfDelimiter];
+            var numbers = delimitedNumbers.Split(delimiterUsed);
+            return numbers.Aggregate(0, (current, number) => current + int.Parse(number));
+        }
+    }
+}
